c5ca1b6faa79d1d59ce3053e7973a505cde31c73
Marwane Sabri <marwanecompany@gmail.com>
2022-08-18 14:06:09 +0200

    ajout du front

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+const path = require('path');
 const app = express();
 
-mongoose.connect('mongodb+srv://sabri_861:azerty861@cluster0.ayyum.mongodb.net/?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
- app.get("/",(req, res)=>{
-    res.status(200).json({mesage: "ok clément"})
- })
 
+
+mongoose.connect('mongodb+srv://sabri_861:azerty861@cluster0.ayyum.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+    app.use(express.json());
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 module.exports = app;
